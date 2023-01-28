68e6f7bfa5438dd16fbc3ecf1ff4d069eb760a05
huangruishen <2394790117@qq.com>
2023-01-28 23:01:59 +0800

    compile

--- ramfs.c
+++ ramfs.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
 #define max_fd 4096
 #define max_fileanddir 65536
 #define length_name 32
@@ -10,42 +11,59 @@
 typedef struct node {
     enum {
         FILE_NODE = 1,
-        DIR_NODE  = 2
-    }type;
+        DIR_NODE = 2
+    } type;
     struct node *child;
     struct node *sibling;
     void *contnet;
-//    int number_child;
     int size;
     char *shortname;
-}node;
+} node;
 node *root;
-typedef struct filedesc{
+typedef struct filedesc {
     bool use;
     int offset;
     int flags;
     int writable;
     int readable;
     node *file;
-}filedesc;
+} filedesc;
 filedesc filed[max_fd];
+
 int pathname_simple(char **str, char *temp_string, const char *pathname) {
     int lengthofroad = strlen(pathname);
-    if(*pathname == '/' && lengthofroad <= length_road){
+    if (*pathname == '/' && lengthofroad <= length_road) {
         int index = 0;
         char *temp = malloc(length_name + 3);
         temp = strtok(temp_string, "/");
-        while (temp != NULL){
-            if(strlen(temp) > length_name) {
+        while (temp != NULL) {
+            if (strlen(temp) > length_name) {
                 free(temp);
                 str = NULL;
                 return 0;
             }
             str[index] = temp;
-            temp = strtok(NULL,"/");
-            index ++;
+            temp = strtok(NULL, "/");
+            index++;
         }
-        if(pathname[lengthofroad - 1] == '/' && (strtok(str[index - 1],".text") !=NULL)) {
+        if(index >= 2) {
+            for (int i = 0; i <= index - 2 ; i++) {
+                int count_ = strlen((str[i]));
+                if(count_ >= 4
+                   && str[i][count_ - 1] == 't'
+                   && str[i][count_ - 2] == 'x'
+                   && str[i][count_ - 3] == 't'
+                   && str[i][count_ - 4] == '.'){
+                    free(temp);
+                    return 0;
+                }
+            }
+        }
+        int count = strlen(str[index - 1]);
+        if (pathname[lengthofroad - 1] == '/' && count >= 4
+            && str[index - 1][count - 1] == 't'
+            && str[index - 1][count - 2] == 'x'
+            && str[index - 1][count - 3] == 't' && str[index - 1][count - 4] == '.') {
             free(temp);
             str = NULL;
             return 0;
@@ -57,12 +75,13 @@ int pathname_simple(char **str, char *temp_string, const char *pathname) {
         return 0;
     }
 }
+
 int ropen(const char *pathname, int flags) {
     char *temp_string = malloc(length_road + 1);
     strcpy(temp_string, pathname);
     char **str = malloc(length_road + 1);
     int index = pathname_simple(str, temp_string, pathname);
-    if(str == NULL) {
+    if (index == 0) {
         return -1;
     }
     // todo
@@ -73,19 +92,19 @@ int ropen(const char *pathname, int flags) {
 
 
 int rclose(int fd) {
-  // TODO();
+    // TODO();
 }
 
 ssize_t rwrite(int fd, const void *buf, size_t count) {
-  // TODO();
+    // TODO();
 }
 
 ssize_t rread(int fd, void *buf, size_t count) {
-  // TODO();
+    // TODO();
 }
 
 off_t rseek(int fd, off_t offset, int whence) {
-  // TODO();
+    // TODO();
 }
 
 int rmkdir(const char *pathname) {
@@ -93,60 +112,56 @@ int rmkdir(const char *pathname) {
     strcpy(temp_string, pathname);
     char **str = malloc(length_road + 1);
     int index = pathname_simple(str, temp_string, pathname);
-    if(str == NULL) {
+    if (/*str == NULL*/index == 0) {
         return -1;
     }
-    node *instruction = malloc(sizeof (struct node) + 5);
-    instruction = root->child;
-//    instruction->sibling = NULL;
-//    instruction->child = NULL;
-//    int number_low = instruction->number_child;
-    for (int i = 0; i <= index - 1 ; i ++) {
+    int count = strlen(str[index - 1]);
+    if (count >= 4 && str[index - 1][count - 1] == 't' && str[index - 1][count - 2] == 'x'
+        && str[index - 1][count - 3] == 't' && str[index - 1][count - 4] == '.') {
+        return -1;
+    }//判断最后类型是目录
+    node *instruction = malloc(sizeof(struct node) + 5);
+    instruction = root->child;//从头遍历链表
+    for (int i = 0; i <= index - 1; i++) {
         if (i == index - 1) {
-            for (; ; ) {
-                if(instruction->sibling == NULL && instruction->shortname != NULL)
-                if (strcmp(str[i], instruction->shortname) == 0) {
-                    return -1;
-                }
-                if (instruction->sibling == NULL) {
-                    break;
+            for (;;) {
+                if (instruction->shortname != NULL) {//判断链表自身是否为空（只有第一次循环有用）
+                    if (strcmp(str[i], instruction->shortname) == 0) {
+                        return -1;
+                    }
                 }
                 instruction = instruction->sibling;
-            }//检查没有重名的
-            if(instruction->shortname != NULL) {
+                if (instruction->sibling == NULL) {//（之后循环退出的条件）
+                    break;
+                }
+            }//检查有没有重名的
+            if (instruction->shortname != NULL) {//判断链表自身不为空并引入新节点
                 instruction->sibling = malloc(sizeof(struct node));
                 instruction->sibling->sibling = NULL;
-                instruction->sibling->shortname = malloc(strlen(str[i]));
+                instruction->sibling->shortname = malloc(strlen(str[i]) + 1);
                 strcpy(instruction->sibling->shortname, str[i]);
-                if ((strtok(str[index - 1], ".text") != NULL)) {
-                    instruction->sibling->type = DIR_NODE;
-                } else {
-                    instruction->sibling->type = FILE_NODE;
-                }
+                instruction->sibling->type = DIR_NODE;
                 return 0;
-            }else{
+            } else {//判断链表自身为空
                 instruction->sibling = NULL;
-                instruction->shortname = malloc(strlen(str[i]));
+                instruction->shortname = malloc(strlen(str[i]) + 1);
                 strcpy(instruction->shortname, str[i]);
-                if ((strtok(str[index - 1], ".text") != NULL)) {
-                    instruction->type = DIR_NODE;
-                } else {
-                    instruction->type = FILE_NODE;
-                }
+                instruction->type = DIR_NODE;
                 return 0;
             }
         }
-        for (; ; ) {
-            if(strcmp(str[i],instruction->shortname) == 0){
-                if(instruction->type == DIR_NODE) {
-//                    number_low = instruction->number_child;
-                    instruction = instruction->child;
-                    continue;
-                }else{
-                    return -1;
+        if (instruction->shortname == NULL) {
+            return -1;
+        }
+        for (;;) {
+            if (strcmp(str[i], instruction->shortname) == 0) {
+                if (instruction->child == NULL) {
+                    instruction->child = malloc(sizeof(struct node));
                 }
+                instruction = instruction->child;
+                break;
             }
-            if(instruction->sibling == NULL) {
+            if (instruction->sibling == NULL) {
                 return -1;
             }
             instruction = instruction->sibling;
@@ -156,16 +171,21 @@ int rmkdir(const char *pathname) {
     free(temp_string);
     free(str);
 }
+
 int rrmdir(const char *pathname) {
 
     char *temp_string = malloc(length_road + 1);
     strcpy(temp_string, pathname);
     char **str = malloc(length_road + 1);
     int index = pathname_simple(str, temp_string, pathname);
-    if(str == NULL) {
+    if (index == 0) {
         return -1;
     }
-    // todo
+    node *instruction = malloc(sizeof(struct node) + 5);
+    instruction = root->child;
+    for (int i = 0; i <= index - 1; i++) {
+
+    }
 
     free(temp_string);
     free(str);
@@ -176,7 +196,7 @@ int runlink(const char *pathname) {
     strcpy(temp_string, pathname);
     char **str = malloc(length_road + 1);
     int index = pathname_simple(str, temp_string, pathname);
-    if(str == NULL) {
+    if (index == 0) {
         return -1;
     }
     // todo
@@ -186,10 +206,9 @@ int runlink(const char *pathname) {
 }
 
 void init_ramfs() {
-    root = malloc(sizeof (struct node));
+    root = malloc(sizeof(struct node));
     root->type = DIR_NODE;
     root->shortname = "/";
-//    root->number_sibling = 0;
-    root->sibling = malloc(sizeof (struct node));
-    root->child = malloc(sizeof (struct node));
+    root->sibling = NULL;
+    root->child = malloc(sizeof(struct node));
 }
