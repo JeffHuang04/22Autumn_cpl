ef25b97ec57a3f67f3134632bb49f2723abfb581
huangruishen <2394790117@qq.com>
2023-02-07 21:50:43 +0800

    compile

--- ramfs.c
+++ ramfs.c
@@ -37,7 +37,6 @@ filedesc filed[max_fd + 1];
 void freestr(char **str,int index){
     if(index >= 1) {
         for (int i = 0; i <= index - 1; i++) {
-            //char *temp = str[index];
             free(str[i]);
             str[i] = NULL;
         }
@@ -108,7 +107,7 @@ int ropen(const char *pathname, int flags) {
     }
     char **str = NULL;
     str = malloc( (max_deepth_think + 1)*sizeof(char*));
-    memset(str,0,(max_deepth_think)*sizeof(char*));
+    memset(str,0,(max_deepth_think + 1)*sizeof(char*));
     char *temp_pathname = NULL;
     temp_pathname = malloc(length_pathname + 1);
     memset(str,0,length_pathname + 1);
@@ -403,7 +402,7 @@ int rmkdir(const char *pathname) {
     }
     char **str = NULL;
     str = malloc( (max_deepth_think + 1)*sizeof(char*));
-    memset(str,0,(max_deepth_think)*sizeof(char*));
+    memset(str,0,(max_deepth_think + 1)*sizeof(char*));
     char *temp_pathname = NULL;
     temp_pathname = malloc(length_pathname + 1);
     memset(temp_pathname,0,length_pathname + 1);
@@ -489,7 +488,7 @@ int rrmdir(const char *pathname) {
     }
     char **str = NULL;
     str = malloc( (max_deepth_think + 1)*sizeof(char*));
-    memset(str,0,(max_deepth_think)*sizeof(char*) );
+    memset(str,0,(max_deepth_think + 1)*sizeof(char*) );
     char *temp_pathname = NULL;
     temp_pathname = malloc(length_pathname + 1);
     memset(temp_pathname,0,length_pathname + 1);
@@ -615,7 +614,7 @@ int runlink(const char *pathname) {
     }
     char **str = NULL;
     str = malloc( (max_deepth_think + 1)*sizeof(char*));
-    memset(str,0,(max_deepth_think)*sizeof(char*));
+    memset(str,0,(max_deepth_think + 1)*sizeof(char*));
     char *temp_pathname = NULL;
     temp_pathname = malloc(length_pathname + 1);
     memset(temp_pathname , 0 , length_pathname + 1);
